a35a7b6c60dc48bb001cc4405ed16fdc7c780b29
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-28 20:55:01 +0700

    first commit

--- driver/boot/master.sh
+++ driver/boot/master.sh
@@ -4,6 +4,6 @@ set -e
 
 cd "$(dirname "$0")"
 
-docker compose --env-file ../../.env -f ../../docker-compose.yml up -d --build --force-recreate
+docker compose --env-file ../../.env -f ../../server/portainer/docker-compose.yml up -d --build --force-recreate
 
 echo "Application boot successfully!"
--- driver/shutdown/master.sh
+++ driver/shutdown/master.sh
@@ -4,6 +4,6 @@ set -e
 
 cd "$(dirname "$0")"
 
-docker compose --env-file ../../.env -f ../../docker-compose.yml down -v --remove-orphans
+docker compose --env-file ../../.env -f ../../server/portainer/docker-compose.yml down -v --remove-orphans
 
 echo "Application shutdown successfully!"
